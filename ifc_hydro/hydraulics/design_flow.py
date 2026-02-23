443d6f72f699695971510d21f5321b96f2e652c5
andrebmuller <andrebuchmannmuller@gmail.com>
2026-02-23 10:51:35 -0300

    fix: flow calculations updated to comply with square root method

--- ifc_hydro/hydraulics/design_flow.py
+++ ifc_hydro/hydraulics/design_flow.py
@@ -7,7 +7,7 @@ standardized terminal flow rates.
 """
 
 from ..core.base import Base
-from .input_tables import design_flow_table
+from .input_tables import score_table
 
 
 class DesignFlow:
@@ -36,23 +36,23 @@ class DesignFlow:
         Returns:
             list: Flow rates for each component in each path
         """
-        # Calculate cumulative flow for each component in each path
-        flow_list = []
+        # Calculate cumulative score for each component in each path
+        score_list = []
         n = 0
 
 
         for path in all_paths:
-            flow_list.append([])
+            score_list.append([])
             i = -1
             for component in path:
                 # Base.append_log(self, f"> Processing component with ID {component[0]} in path {n}...")
                 if component.is_a() == "IfcSanitaryTerminal":
                     # Assign design flow for terminals
-                    flow_list[n].append((component[0], design_flow_table[component[8]]))
+                    score_list[n].append((component[0], score_table[component[8]]))
                 else:
                     # Propagate flow from previous component
-                    flow_list[n].append((component[0], flow_list[n][i][1]))
+                    score_list[n].append((component[0], score_list[n][i][1]))
                 i += 1
             n += 1
 
-        return flow_list
+        return score_list
